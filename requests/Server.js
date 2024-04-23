983bc9ed21e8179ef230a243495250d07b87abf7
Witcher-MTM <to4nosergey@gmail.com>
2024-04-23 14:41:57 +0300

    Update Server.js

--- requests/Server.js
+++ requests/Server.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 class Server{
     async getUserData(phoneNumber){
         try {
-            const response = await axios.get(`https://curious-pinafore-goat.cyclic.app/user/data/${phoneNumber}`);
+            const response = await axios.get(`https://gosserver3-production.up.railway.app/user/data/${phoneNumber}`);
             return response;
         } catch (error) {
             console.log('Ошибка при получении данных с сервера:', error.response.data);
@@ -11,7 +11,7 @@ class Server{
     }
     async getUsers(){
         try {
-            const response = await axios.get(`https://curious-pinafore-goat.cyclic.app/user`);
+            const response = await axios.get(`https://gosserver3-production.up.railway.app/user`);
             return response.data;
         } catch (error) {
             console.error('Ошибка при получении данных с сервера:', error);
